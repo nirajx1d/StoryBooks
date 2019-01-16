110b176154b6a6efa90c05d4652a665a65afbc7d
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2019-01-16 20:16:25 +0530

    google oauth working

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const passport = require('passport');
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('Working');
 });
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://Nirajx1d:niraj@123@ds159204.mlab.com:59204/storybook-dev',
+    googleClientID: '779543466160-g1fgp2ls7ftij85bdv8a5hk4ikmrngn3.apps.googleusercontent.com',
+    googleClientSecret: 'lcoKqcKeaA4VF3lQGpaafkHv'
+};  
