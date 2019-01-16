05ae319563d94cf33e59e3eafc18ca8c3ee152bf
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2019-01-16 13:20:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Working');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
