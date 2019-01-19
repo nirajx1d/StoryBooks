5437c6f9c71adb47211f2c798f6e860f95d9bc06
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2019-01-19 23:31:13 +0530

    Edit access control added

--- routes/stories.js
+++ routes/stories.js
@@ -9,6 +9,7 @@ const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 router.get('/', (req, res) => {
     Story.find({ status: 'public' })
         .populate('user')
+        .sort({date: 'desc'})
         .then(stories => {
             res.render('stories/index', {
                 stories: stories
@@ -42,9 +43,14 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
         _id: req.params.id
     })
     .then(story => {
-        res.render('stories/edit',{
-            story: story
-        });
+        if(story.user != req.user.id) {
+            res.redirect('/stories');
+        } else {
+            res.render('stories/edit',{
+                story: story
+            });
+        }
+        
     });
 }); 
 
